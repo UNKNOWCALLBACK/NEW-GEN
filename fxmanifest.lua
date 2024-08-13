d978d0b3abf3f203f891b5478e4178734b7be849
UNKNOWCALLBACK_TEAM <147795365+UNKNOWCALLBACK@users.noreply.github.com>
2024-08-13 07:39:13 +0000

    edit path

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,20 +4,23 @@ game 'gta5'
 
 lua54 'yes'
 
-description 'ESX Identity'
+description 'ESX Identity edit by unknowcallback'
 
 version '1.2.0'
 
 server_scripts {
+			'config/*.lua',
+	'function/server.lua'
 	'@mysql-async/lib/MySQL.lua',
 --    '@mongodb/lib/MongoDB.lua', --กรณีใช้mongodbให้ลบบรรทัด 12 ออกและเปิด commant นี้
 
 }
 
 client_scripts {
-	'config.lua',
-	'client/main.lua'
+	'config/*.lua',
+	'function/client.lua'
 }
+
 
 ui_page "web/ui.html"
 
